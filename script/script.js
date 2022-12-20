71877eaac9fb3fd42d997faf3ed8191a68e78d3e
RobertB512 <rwbcool@gmail.com>
2022-12-20 10:10:22 -0800

    Add hamburger menu with javascript

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let hamburger = document.querySelector("nav");
+hamburger.style.right = "-100vw";
+
+function hamburgerMenu() {
+  console.log(`right-hidden: ${hamburger.style.right === "-100vw"}`)
+  console.log(`in view: ${hamburger.style.right === "0"}`)
+
+  if (hamburger.style.right === "-100vw") {
+    hamburger.style.right = "0";
+    console.log("should be visible")
+  } else if (hamburger.style.right = "0") {
+    hamburger.style.right = "-100vw";
+    console.log("should be hidden")
+  }
+}
+
+
+
+
