0e3d4887e8617cf3f1dd66cdc531a528e11f668b
RobertB512 <rwbcool@gmail.com>
2023-01-30 12:08:46 -0800

    Make menu toggle from "display none" to block

--- script/script.js
+++ script/script.js
@@ -1,19 +1,5 @@
-let hamburger = document.querySelector("nav");
-hamburger.style.right = "-100vw";
-
-function hamburgerMenu() {
-  // console.log(`right-hidden: ${hamburger.style.right === "-100vw"}`)
-  // console.log(`in view: ${hamburger.style.right === "0"}`)
-
-  if (hamburger.style.right === "-100vw") {
-    hamburger.style.right = "0";
-    // console.log("should be visible")
-  } else if (hamburger.style.right = "0") {
-    hamburger.style.right = "-100vw";
-    // console.log("should be hidden")
-  }
-}
-
-
-
-
+let hamburger = document.querySelector(".menu-nav-btn");
+let navigation = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+	navigation.classList.toggle("show-nav");
+});
